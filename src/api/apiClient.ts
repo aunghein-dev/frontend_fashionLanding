1c9b5041c1f9d6415d8865df561881a9f5d7ffc3
Aung Hein <aunghein.mm@hotmail.com>
2025-09-19 15:42:24 +0630

    Frontend fixes and updates for v1.0

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,8 +1,10 @@
 // apiClient.ts
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 const apiClient = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
